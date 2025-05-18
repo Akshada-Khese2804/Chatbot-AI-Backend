e7d6dbaca866e8cfea346471f11c418a7f852314
Akshada-Khese2804 <akshadakhese100@gmail.com>
2025-05-18 17:57:43 +0530

    Add files via upload

---
+++ database/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    //   dbName: "ChatbotYoutube", 
+    });
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
